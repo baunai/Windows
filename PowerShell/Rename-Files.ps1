b4c2f954b3dde44918d712f85208a25dcab35d05
baunai <75685937+baunai@users.noreply.github.com>
2022-10-26 22:37:13 -0500

    Update Rename-Files.ps1

--- PowerShell/Rename-Files.ps1
+++ PowerShell/Rename-Files.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -Path 'D:\Hoang-Music' -Filter "1412 *.mp3" -Recurse | Rename-Item -NewName {$_.Name -replace '1412 ',''}
+Get-ChildItem -Path 'D:\MusicFolder' -Filter "1412 *.mp3" -Recurse | Rename-Item -NewName {$_.Name -replace '1412 ',''}
